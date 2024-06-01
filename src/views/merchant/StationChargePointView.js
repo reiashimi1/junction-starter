da709a1ccfa34bec1d45305f4e3494e45a29f66a
Rei Ashimi <reiashimi1@gmail.com>
2024-06-01 15:51:24 +0200

    Anxhelo commit dont push pa e pare

---
+++ src/views/merchant/StationChargePointView.js
@@ -0,0 +1,12 @@
+import Layout from "@/layouts/MerchantLayout/Layout";
+
+const StationChargePoint = () => {
+
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default StationChargePoint;
